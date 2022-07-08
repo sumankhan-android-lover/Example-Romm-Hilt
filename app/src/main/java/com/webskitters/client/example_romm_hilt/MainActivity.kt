42192751bfc3ecbc571519d3cf2a618c9b6073c8
Suman Khan <suman.khan@webskitters.com>
2022-07-08 17:50:23 +0530

    init commit

---
+++ app/src/main/java/com/webskitters/client/example_romm_hilt/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.webskitters.client.example_romm_hilt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
