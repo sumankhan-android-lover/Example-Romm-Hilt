46a77763e9ad165fd83f2129a187babd8b7af8f2
Suman Khan <suman.khan@webskitters.com>
2022-07-11 13:10:35 +0530

    init gradle file update

---
+++ app/src/main/java/com/webskitters/client/example_romm_hilt/Application.kt
@@ -0,0 +1,4 @@
+package com.webskitters.client.example_romm_hilt
+
+class Application {
+}
--- app/src/main/java/com/webskitters/client/example_romm_hilt/ui/MainActivity.kt
+++ app/src/main/java/com/webskitters/client/example_romm_hilt/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.webskitters.client.example_romm_hilt
+package com.webskitters.client.example_romm_hilt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.webskitters.client.example_romm_hilt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
